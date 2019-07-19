37c5ee7a8403ed3e6bbaf63a2aa838f5e38cc602
KateDay <khummle@gmail.com>
2019-07-19 08:49:50 -0600

    set up request to example.org

--- https_example.js
+++ https_example.js
@@ -1,3 +1,15 @@
 const https = require('https');
 
-console.log('I did it!');
+const options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+let callback = function(){
+  console.log('In response handler callback!');
+}
+
+console.log('Im about to make the request!');
+
+https.request(options, callback).end();
+console.log("I've made the request!");
